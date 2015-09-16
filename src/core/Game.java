ef98a570e5fe757115357fbe30517938ba98b036
Axel Mousset <pymzorr@gmail.com>
2015-09-16 11:10:24 +0200

    Add player init; Add Game.getPlayer() and Game.start(); Fix indent issues

--- src/core/Game.java
+++ src/core/Game.java
@@ -10,38 +10,42 @@ public class Game {
 	private Vector<Player> players;
 	private int playerNumber = 0;
 
-
-	public Game(int playerNumber) {
-		this.players = new Vector<Player>();
+    public Game(int playerNumber) {
+    	this.players = new Vector<Player>();
     	this.playerNumber = playerNumber;
 
     	for (int i = 0; i < playerNumber; i++) {
-    		players.add(new Player());
+    		Player newPlayer = new Player();
+    		newPlayer.drawCard(4);
+    		players.add(newPlayer);
     	}
 
     	this.currentPlayer = this.players.get(0);
     }
 
-
+    public void start() {
+    	this.running = true;
+    }
+    
     public void nextTurn(Card card, ActionType action, Player player) {
-        this.currentPlayer.playCard();
+        this.currentPlayer.playCard(card, action, player);
     }
 
-	public Player getPlayer(int playerId) {
-		return players.get(playerId);
-	}
+    public Player getPlayer(int playerId) {
+    	return players.get(playerId);
+    }
 
     public boolean isRunning() {
         return this.running;
     }
 
 
-	public Player getCurrentPlayer() {
-		return this.currentPlayer;
-	}
+    public Player getCurrentPlayer() {
+    	return this.currentPlayer;
+    }
 
 
-	public int getPlayerNumber() {
-		return this.playerNumber;
-	}
+    public int getPlayerNumber() {
+    	return this.playerNumber;
+    }
 }
