16628173293eb1296b751e428361649125a0c630
Gabriel Juchault <gabriel.juchault@gmail.com>
2015-09-26 19:56:02 +0200

    String values for seasons

--- src/core/SeasonType.java
+++ src/core/SeasonType.java
@@ -5,8 +5,36 @@ package core;
  * Lists four seasons
  */
 public enum SeasonType {
-	SPRING,
-	SUMMER,
-	FALL,
-	WINTER
+	SPRING("Été"),
+	SUMMER("Printemps"),
+	FALL("Automne"),
+	WINTER("Hiver");
+
+	private final String name;
+
+	/**
+	 * Store the real season name
+	 * @param s The real season name
+	 */
+    private SeasonType(String s) {
+        this.name = s;
+    }
+
+    /**
+     * Check for equality between two seasons
+     * @param otherName The other season name
+     * @return True if equals
+     */
+    public boolean equalsName(String otherName) {
+        return (otherName == null) ? false : this.name.equals(otherName);
+    }
+
+    /**
+     * Get the real season name
+     * @return The real season name
+     */
+    @Override
+	public String toString() {
+       return this.name;
+    }
 }
